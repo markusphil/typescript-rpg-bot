85e3774fdbec1d2ccc63c9a1903ba0a8e1307374
markusphil <m.pilipp1991@web.de>
2019-09-29 16:40:20 +0200

    dealing with ChannelTypes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import Discord from 'discord.js';
+import * as Discord from 'discord.js';
 
 const client = new Discord.Client();
 
-client.once('ready', () => console.log('ready!'));
+let rpgChannel: Discord.TextChannel;
+
+client.once('ready', () => {
+  console.log('ready!');
+  const targetChannel: Discord.Channel | undefined = client.channels.get('627866917394317334');
+  if (targetChannel instanceof Discord.TextChannel) rpgChannel = targetChannel;
+  if (targetChannel) {
+    rpgChannel.send('I am awake');
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
